728f9d9f4e23f773567e17dab12b4340b47276f5
m-niemiec <michal.niemiec.it@gmail.com>
2020-06-12 08:39:29 +0200

    Fixed some RWD issues, updated css for image width, now not used categories will be hidden in notes.

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,9 +8,12 @@ class Project(models.Model):
     description = models.TextField(max_length=1000)
     details = models.TextField(max_length=10000, blank=True)
     screenshot_main = models.ImageField(upload_to="portfolio/images/", blank=True)
-    screenshot_1 = models.ImageField(upload_to="portfolio/images/", blank=True)
-    screenshot_2 = models.ImageField(upload_to="portfolio/images/", blank=True)
-    screenshot_3 = models.ImageField(upload_to="portfolio/images/", blank=True)
+    screenshot_1 = models.ImageField(upload_to="portfolio/images/", blank=True, null=True)
+    screenshot_2 = models.ImageField(upload_to="portfolio/images/", blank=True, null=True)
+    screenshot_3 = models.ImageField(upload_to="portfolio/images/", blank=True, null=True)
     created = models.DateTimeField(blank=True)
     url_github = models.URLField(blank=True)
     url_live = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
