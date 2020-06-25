47d9281dc864671234df87b8f21073b084c476a0
Michal <michal.niemiec.it@gmail.com>
2020-06-25 09:10:34 +0000

    Added new domain

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['michalniemiec.eu.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.michalniemiec.info']
 
 
 # Application definition
